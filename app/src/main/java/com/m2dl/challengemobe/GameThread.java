6c991653eac3d30568b7146ff3b6e94e84240abc
remij <remi.jacques@master-developpement-logiciel.fr>
2021-03-19 08:58:37 +0100

    Copy from last project

--- app/src/main/java/com/m2dl/challengemobe/GameThread.java
+++ app/src/main/java/com/m2dl/challengemobe/GameThread.java
@@ -1,4 +1,50 @@
 package com.m2dl.challengemobe;
 
+import android.graphics.Canvas;
+import android.os.Build;
+import android.view.SurfaceHolder;
+
+import androidx.annotation.RequiresApi;
+
 public class GameThread extends Thread {
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private Canvas canvas;
+    private boolean running;
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+
+    @Override
+    public void run(){
+        while(running) {
+            try {
+                sleep(16,70);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {;
+                    this.gameView.draw(canvas);
+                }
+            } catch (Exception e) {
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/m2dl/challengemobe/GameView.java
+++ app/src/main/java/com/m2dl/challengemobe/GameView.java
@@ -1,14 +1,103 @@
 package com.m2dl.challengemobe;
-
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GameView extends SurfaceView {
-    GameThread thread;
+import androidx.annotation.NonNull;
+import androidx.core.graphics.ColorUtils;
 
-    public GameView(Context context) {
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private GameThread thread;
+    private MainActivity context;
+    private Point circlePosition;
+    private int contextHeight;
+    private int contextWidth;
+    private SharedPreferences sharedPref;
+
+
+    public GameView(MainActivity context) {
         super(context);
+        this.context = context;
+        setFocusable(true);
+        getHolder().addCallback(this);
+        this.sharedPref = context.getPreferences(Context.MODE_PRIVATE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        context.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        contextHeight = displayMetrics.heightPixels;
+        contextWidth = displayMetrics.widthPixels;
+        circlePosition = new Point(contextWidth / 2, contextHeight / 2);
+
+
     }
 
+    public GameThread getThread() {
+        return thread;
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        thread = new GameThread(getHolder(), this);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (canvas != null) {
+            canvas.drawColor(Color.WHITE);
+            Paint paint = new Paint();
+
+
+            /*float lightValue = context.getLightValue();
+            float maxLight = 1200;
+
+
+            float hue = (lightValue / maxLight) * 359.f;
+            int rgb = ColorUtils.HSLToColor(new float[]{hue, 1f, .6f});
+            int red = Color.red(rgb);
+            int green = Color.green(rgb);
+            int blue = Color.blue(rgb);
+            *
+
+            paint.setColor(Color.rgb(red, green, blue));
+
+             */
+            canvas.drawCircle(circlePosition.x, circlePosition.y, 100, paint);
+        }
+    }
+
+    public Point getCirclePosition() {
+        return circlePosition;
+    }
+
+    public void setCirclePosition(Point circlePosition) {
+        this.circlePosition = circlePosition;
+    }
 
 }
